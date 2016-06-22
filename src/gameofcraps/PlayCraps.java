3e328de6fd2aa177a2ad83428d84ac41c4723d05
Eriksen Costa <eriksencosta@gmail.com>
2016-06-22 19:58:42 -0300

    Print statistics in command line interface

--- src/gameofcraps/PlayCraps.java
+++ src/gameofcraps/PlayCraps.java
@@ -14,12 +14,27 @@ public class PlayCraps {
 	}
 
 	public static void main(String[] args) {
-		CrapsGame game = new CrapsGame();
-		int numGames;
+		int numGamesToPlay;
 
 		System.out.println("Welcome to Craps!");
 		System.out.println("Enter number of games: ");
-		numGames = Integer.parseInt(args[0]);
+		numGamesToPlay = Integer.parseInt(args[0]);
+
+		PlayCraps playCraps = new PlayCraps();
+		GameStatistics statistics = playCraps.play(numGamesToPlay);
+
+		System.out.println("Number of games played: " + statistics.gamesPlayed);
+		System.out.println("Number of wins: " + statistics.gamesWon);
+		System.out.println("Length of the longest game played: " + statistics.maximalGameLength);
+		System.out.format("Estimated probability of winning: %.4f\n", statistics.probabilityOfWinning());
+		System.out.println("Total number of rolls: " + statistics.totalRolls);
+		System.out.format("Average number of rolls per game: %.4f\n", statistics.averageOfRollsPerGame());
+		System.out.println("Number of wins on coming out roll: " + statistics.winsOnComingOutRoll);
+		System.out.format("Estimated probability of winning on coming out roll: %.4f\n",
+				statistics.probabilityOfWinningOnComingOutRoll());
+		System.out.println("Number of losses on coming out roll: " + statistics.lossesOnComingOutRoll);
+		System.out.format("Estimated probability of losing on coming out roll: %.4f\n",
+				statistics.probabilityOfLosingOnComingOutRoll());
 	}
 
 	public GameStatistics play(int numGamesToPlay) {
