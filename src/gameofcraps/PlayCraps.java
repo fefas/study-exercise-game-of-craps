e49cf3acdad0887eff13b908ca08e673309b12fd
Eriksen Costa <eriksencosta@gmail.com>
2016-06-22 19:20:33 -0300

    Implement game statistics calculation for one game

--- src/gameofcraps/PlayCraps.java
+++ src/gameofcraps/PlayCraps.java
@@ -1,6 +1,17 @@
 package gameofcraps;
 
+import java.util.Arrays;
+
 public class PlayCraps {
+	private Dice dice;
+
+	public PlayCraps() {
+		this(new Dice());
+	}
+
+	public PlayCraps(Dice dice) {
+		this.dice = dice;
+	}
 
 	public static void main(String[] args) {
 		CrapsGame game = new CrapsGame();
@@ -12,10 +23,32 @@ public class PlayCraps {
 	}
 
 	public GameStatistics play(int numGames) {
-		CrapsGame game = new CrapsGame();
+		CrapsGame game = new CrapsGame(dice);
+		int gamesWon = 0;
+		int maximalGameLength = 0;
+		int totalRolls = 0;
+		int winsOnComingOutRoll = 0;
+		int lossesOnComingOutRoll = 0;
 
 		game.play();
 
-		return new GameStatistics(numGames, 0, 0, 0, 0, 0);
+		if (game.getWin()) {
+			gamesWon++;
+			if (game.getNumRolls() == 1) {
+				winsOnComingOutRoll++;
+			}
+		} else {
+			if (game.getNumRolls() == 1) {
+				lossesOnComingOutRoll++;
+			}
+		}
+
+		totalRolls += game.getNumRolls();
+		if (game.getNumRolls() > maximalGameLength) {
+			maximalGameLength = game.getNumRolls();
+		}
+
+		return new GameStatistics(numGames, gamesWon, maximalGameLength, totalRolls, winsOnComingOutRoll,
+				lossesOnComingOutRoll);
 	}
 }
--- test/gameofcraps/PlayCrapsTest.java
+++ test/gameofcraps/PlayCrapsTest.java
@@ -2,21 +2,59 @@ package gameofcraps;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
-import org.junit.Ignore;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class PlayCrapsTest {
 
-	@Test
-	@Ignore("Depends on GameStatistics implementation")
-	public void playingCrapsGeneratesGameStatistics() {
-		final int numGames = 1;
-		PlayCraps playing = new PlayCraps();
-
-		GameStatistics statistics = playing.play(numGames);
-
-		assertEquals(numGames, statistics.gamesPlayed);
-		assertTrue(statistics.gamesWon <= numGames);
+	@Parameters
+	public static Collection<Object[]> diceAndExpectedStatistics() {
+		return Arrays.asList(new Object[][] {
+			{
+				new SequencedDice(4, 5, 7),
+				new GameStatistics(1, 0, 3, 3, 0, 0)
+			},
+			{
+				new SequencedDice(4, 5, 4),
+				new GameStatistics(1, 1, 3, 3, 0, 0)
+			},
+			{
+				new SequencedDice(11),
+				new GameStatistics(1, 1, 1, 1, 1, 0)
+			},
+			{
+				new SequencedDice(2),
+				new GameStatistics(1, 0, 1, 1, 0, 1)
+			}
+		});
 	}
 
+	private Dice dice;
+	private GameStatistics expectedStatistics;
+
+	public PlayCrapsTest(Dice dice, GameStatistics expectedStatistics) {
+		this.dice = dice;
+		this.expectedStatistics = expectedStatistics;
+	}
+
+	@Test
+	public void statisticsAreCorrectWhenSingleGameLost() {
+		final int numGamesToPlay = expectedStatistics.gamesPlayed;
+		PlayCraps playing = new PlayCraps(dice);
+
+		GameStatistics statistics = playing.play(numGamesToPlay);
+
+		assertEquals(expectedStatistics.gamesPlayed, statistics.gamesPlayed);
+		assertEquals(expectedStatistics.gamesWon, statistics.gamesWon);
+		assertEquals(expectedStatistics.maximalGameLength, statistics.maximalGameLength);
+		assertEquals(expectedStatistics.totalRolls, statistics.totalRolls);
+		assertEquals(expectedStatistics.winsOnComingOutRoll, statistics.winsOnComingOutRoll);
+		assertEquals(expectedStatistics.lossesOnComingOutRoll, statistics.lossesOnComingOutRoll);
+	}
 }
